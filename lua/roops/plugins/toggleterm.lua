2a264c0a99afed0a1335121801d9faf482cb786c
roopsBee <35971830+roopsBee@users.noreply.github.com>
2023-01-13 11:56:45 +0000

    load new terminal on session change

--- lua/roops/plugins/toggleterm.lua
+++ lua/roops/plugins/toggleterm.lua
@@ -15,13 +15,15 @@ toggleterm.setup({
 	direction = "float",
 	close_on_exit = true, -- close the terminal window when the process exits
 	shell = vim.o.shell, -- change the default shell
+	autochdir = true,
+	persist_mode = false,
 	-- This field is only relevant if direction is set to 'float'
 	float_opts = {
 		-- The border key is *almost* the same as 'nvim_open_win'
 		-- see :h nvim_open_win for details on borders however
 		-- the 'curved' border is a custom border type
 		-- not natively supported but implemented in this plugin.
-		border = "curved",
+		border = "double",
 		winblend = 1,
 		highlights = {
 			border = "Normal",
@@ -44,8 +46,7 @@ toggleterm.setup({
 -- vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
 function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
+	Terminal:new({ cmd = "lazygit" }):toggle()
 end
