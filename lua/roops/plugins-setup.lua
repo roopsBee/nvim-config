593068270be1b04abebec8a8bb38606fd8216f41
roops <roops.pesh@gmail.com>
2023-04-28 17:18:55 +0100

    update

--- lua/roops/plugins-setup.lua
+++ lua/roops/plugins-setup.lua
@@ -14,7 +14,7 @@ local packer_bootstrap = ensure_packer() -- true if packer was just installed
 
 -- autocommand that reloads neovim and installs/updates/removes plugins
 -- when file is saved
-vim.cmd([[ 
+vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
