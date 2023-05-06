07f2275c0c7261e35444a49c2a48b6021cad6461
roopsBee <35971830+roopsBee@users.noreply.github.com>
2023-05-06 15:36:05 +0100

    update astro formatting

--- lua/roops/plugins/lsp/null-ls.lua
+++ lua/roops/plugins/lsp/null-ls.lua
@@ -1,11 +1,11 @@
 -- import null-ls plugin safely
 local setup, null_ls = pcall(require, "null-ls")
 if not setup then
-    return
+	return
 end
 
 -- for conciseness
-local formatting = null_ls.builtins.formatting   -- to setup formatters
+local formatting = null_ls.builtins.formatting -- to setup formatters
 local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
 -- to setup format on save
@@ -13,53 +13,47 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- configure null_ls
 null_ls.setup({
-    -- setup formatters & linters
-    sources = {
-        --  to disable file types use
-        --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-        null_ls.builtins.formatting.rustfmt, -- rustfmt
-        formatting.prettier.with({     -- js/ts formatter
-            extra_filetypes = { "astro" },
-        }),
-        formatting.prettier, -- js/ts formatter
-        formatting.stylua, -- lua formatter
-        diagnostics.eslint_d.with({
-            -- js/ts linter
-            -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-            condition = function(utils)
-                return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-            end,
-        }),
-    },
-    -- configure format on save
-    on_attach = function(current_client, bufnr)
-        local is_astro_file = vim.api.nvim_buf_get_option(bufnr, "filetype") == "astro"
+	-- setup formatters & linters
+	sources = {
+		--  to disable file types use
+		--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
+		null_ls.builtins.formatting.rustfmt, -- rustfmt
+		formatting.prettier.with({ -- js/ts formatter
+			extra_filetypes = { "astro" },
+		}),
+		formatting.prettier, -- js/ts formatter
+		formatting.stylua, -- lua formatter
+		diagnostics.eslint_d.with({
+			-- js/ts linter
+			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
+			condition = function(utils)
+				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
+			end,
+		}),
+	},
+	-- configure format on save
+	on_attach = function(current_client, bufnr)
+		local is_astro_file = vim.api.nvim_buf_get_option(bufnr, "filetype") == "astro"
 
-        local function format_astro()
-            if is_astro_file then
-                vim.lsp.buf.format({ bufnr = bufnr })
-                vim.cmd("silent !prettier --write " .. vim.api.nvim_buf_get_name(bufnr))
-                vim.cmd("edit!")
-            end
-        end
-
-        if current_client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({
-                        filter = function(client)
-                            --  only use null-ls for formatting instead of lsp server
-                            return client.name == "null-ls"
-                        end,
-                        bufnr = bufnr,
-                    })
-                end,
-            })
-        else
-            format_astro()
-        end
-    end,
+		if current_client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					if is_astro_file then
+						vim.lsp.buf.format({ bufnr = bufnr })
+					else
+						vim.lsp.buf.format({
+							filter = function(client)
+								--  only use null-ls for formatting instead of lsp server
+								return client.name == "null-ls"
+							end,
+							bufnr = bufnr,
+						})
+					end
+				end,
+			})
+		end
+	end,
 })
