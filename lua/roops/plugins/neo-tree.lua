bcb46ca76824d7201f310816eed07fcb3539bc25
roops <roops.pesh@gmail.com>
2023-06-23 13:35:54 +0100

    auto close neo tree on file select

--- lua/roops/plugins/neo-tree.lua
+++ lua/roops/plugins/neo-tree.lua
@@ -89,78 +89,78 @@ require("neo-tree").setup({
 		highlight_separator_active = "NeoTreeTabSeparatorActive",
 	},
 	--
-	--event_handlers = {
-	--  {
-	--    event = "before_render",
-	--    handler = function (state)
-	--      -- add something to the state that can be used by custom components
-	--    end
-	--  },
-	--  {
-	--    event = "file_opened",
-	--    handler = function(file_path)
-	--      --auto close
-	--      require("neo-tree").close_all()
-	--    end
-	--  },
-	--  {
-	--    event = "file_opened",
-	--    handler = function(file_path)
-	--      --clear search after opening a file
-	--      require("neo-tree.sources.filesystem").reset_search()
-	--    end
-	--  },
-	--  {
-	--    event = "file_renamed",
-	--    handler = function(args)
-	--      -- fix references to file
-	--      print(args.source, " renamed to ", args.destination)
-	--    end
-	--  },
-	--  {
-	--    event = "file_moved",
-	--    handler = function(args)
-	--      -- fix references to file
-	--      print(args.source, " moved to ", args.destination)
-	--    end
-	--  },
-	--  {
-	--    event = "neo_tree_buffer_enter",
-	--    handler = function()
-	--      vim.cmd 'highlight! Cursor blend=100'
-	--    end
-	--  },
-	--  {
-	--    event = "neo_tree_buffer_leave",
-	--    handler = function()
-	--      vim.cmd 'highlight! Cursor guibg=#5f87af blend=0'
-	--    end
-	--  },
-	-- {
-	--   event = "neo_tree_window_before_open",
-	--   handler = function(args)
-	--     print("neo_tree_window_before_open", vim.inspect(args))
-	--   end
-	-- },
-	-- {
-	--   event = "neo_tree_window_after_open",
-	--   handler = function(args)
-	--     vim.cmd("wincmd =")
-	--   end
-	-- },
-	-- {
-	--   event = "neo_tree_window_before_close",
-	--   handler = function(args)
-	--     print("neo_tree_window_before_close", vim.inspect(args))
-	--   end
-	-- },
-	-- {
-	--   event = "neo_tree_window_after_close",
-	--   handler = function(args)
-	--     vim.cmd("wincmd =")
-	--   end
-	-- }
-	--},
+	event_handlers = {
+		--  {
+		--    event = "before_render",
+		--    handler = function (state)
+		--      -- add something to the state that can be used by custom components
+		--    end
+		--  },
+		{
+			event = "file_opened",
+			handler = function(file_path)
+				--auto close
+				require("neo-tree").close_all()
+			end,
+		},
+		--  {
+		--    event = "file_opened",
+		--    handler = function(file_path)
+		--      --clear search after opening a file
+		--      require("neo-tree.sources.filesystem").reset_search()
+		--    end
+		--  },
+		--  {
+		--    event = "file_renamed",
+		--    handler = function(args)
+		--      -- fix references to file
+		--      print(args.source, " renamed to ", args.destination)
+		--    end
+		--  },
+		--  {
+		--    event = "file_moved",
+		--    handler = function(args)
+		--      -- fix references to file
+		--      print(args.source, " moved to ", args.destination)
+		--    end
+		--  },
+		--  {
+		--    event = "neo_tree_buffer_enter",
+		--    handler = function()
+		--      vim.cmd 'highlight! Cursor blend=100'
+		--    end
+		--  },
+		--  {
+		--    event = "neo_tree_buffer_leave",
+		--    handler = function()
+		--      vim.cmd 'highlight! Cursor guibg=#5f87af blend=0'
+		--    end
+		--  },
+		-- {
+		--   event = "neo_tree_window_before_open",
+		--   handler = function(args)
+		--     print("neo_tree_window_before_open", vim.inspect(args))
+		--   end
+		-- },
+		-- {
+		--   event = "neo_tree_window_after_open",
+		--   handler = function(args)
+		--     vim.cmd("wincmd =")
+		--   end
+		-- },
+		-- {
+		--   event = "neo_tree_window_before_close",
+		--   handler = function(args)
+		--     print("neo_tree_window_before_close", vim.inspect(args))
+		--   end
+		-- },
+		-- {
+		--   event = "neo_tree_window_after_close",
+		--   handler = function(args)
+		--     vim.cmd("wincmd =")
+		--   end
+		-- }
+	},
 	default_component_configs = {
 		container = {
 			enable_character_fade = true,
