b2f658a22357b91b8022381f17308b19e894f94c
roopsBee <35971830+roopsBee@users.noreply.github.com>
2023-09-12 16:58:20 +0100

    update keybinds

--- lua/roops/plugins/rust-tools.lua
+++ lua/roops/plugins/rust-tools.lua
@@ -18,11 +18,26 @@ rust_tools.setup({
 		on_attach = function(c, bufnr)
 			inlay_hints.on_attach(c, bufnr)
 			local keymap = vim.keymap
-
 			-- Hover actions
 			keymap.set("n", "<leader>ha", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
 			-- Code action groups
 			keymap.set("n", "<Leader>ga", rust_tools.code_action_group.code_action_group, { buffer = bufnr })
+			-- set keybinds
+			local opts = { noremap = true, silent = true, buffer = bufnr }
+			keymap.set("n", "<leader>lf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+			keymap.set("n", "<leader>ld", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+			keymap.set("n", "<leader>lp", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+			keymap.set("n", "<leader>lt", "<cmd>Lspsaga peek_type_definition<CR>", opts) -- see definition and make edits in window
+			keymap.set("n", "<leader>lg", "<cmd>Lspsaga goto_definition<CR>", opts) -- see definition and make edits in window
+			keymap.set("n", "<leader>li", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+			keymap.set("n", "<leader>la", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+			keymap.set("n", "<leader>lr", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+			keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
+			keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+			keymap.set("n", "<leader>hd", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+			keymap.set("n", "<leader>ll", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+			keymap.set("n", "<leader>lc", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+			vim.keymap.set({ "x", "v" }, "<leader>a", vim.lsp.buf.code_action, opts) -- see available code actions in visual mode
 		end,
 	},
 })
